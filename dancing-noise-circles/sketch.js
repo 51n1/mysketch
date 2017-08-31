2b233851bf956c29aa5a847083c7028a1b2acd14
51n1 <51n1@users.noreply.github.com>
2017-09-01 00:01:59 +0800

    Add rotation of circle

--- dancing-noise-circles/sketch.js
+++ dancing-noise-circles/sketch.js
@@ -3,25 +3,30 @@
 // https://twitter.com/t51n1
 
 var radius;
+var rotation;
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
   radius = width > height ? height*0.4 : width*0.4;
-  background(255);
+  rotation = 0;
 }
 
 function draw() {
+  background(255);
   beginShape();
   for(var i = 0; i < 360; i += 10) {
-    var angle = i;
+    var angle = i - rotation;
     var x = width*0.5 + radius * sin(radians(angle));
     var y = height*0.5 + radius * cos(radians(angle));
 
     stroke(0);
     noFill();
     vertex(x, y);
+    
+    ellipse(x, y, 5, 5);
   }
   endShape(CLOSE);
+  rotation += 0.5;
 
   if (touches.length > 1 || keyIsDown(ENTER)) saveCanvas();
 }
