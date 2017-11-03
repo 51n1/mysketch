c93825ef87233daedeb41bcabe7ddc9fdd0a14b3
51n1 <>
2017-11-04 00:50:30 +0800

    update

--- arrays-of-objects/sketch.js
+++ arrays-of-objects/sketch.js
@@ -1,27 +1,39 @@
-// 7.3: Arrays of Objects - p5.js Tutorial by Daniel Shiffman
+// 7.3: Arrays of Objects
+// 7.4: Mouse Interaction with Objects
+// 7.5: Removing Objects from Arrays
+// - p5.js Tutorial by Daniel Shiffman
 
 let bubbles = [];
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
-  // for (let i = 0; i < 10; i++) {
-  //   let x = random(width);
-  //   let y = random(height);
-  //   let r = random(10, 40);
-  //   bubbles[i] = new Bubble(x, y, r);
-  // }
+  for (let i = 0; i < 10; i++) {
+    let x = random(width);
+    let y = random(height);
+    let r = random(10, 50);
+    let b = new Bubble(x, y, r);
+    bubbles.push(b);
+  }
 }
 
 function mousePressed() {
-  let r = random(10, 50);
-  let b = new Bubble(mouseX, mouseY, r);
-  bubbles.push(b);
+  for (let i = bubbles.length - 1; i >= 0; i--) {
+    if ( bubbles[i].contains(mouseX, mouseY) ) {
+      bubbles.splice(i, 1);
+      return;
+    }
+  }
 }
 
 function draw() {
   background(0);
 
   for (let i = 0; i < bubbles.length; i++) {
+    if ( bubbles[i].contains(mouseX, mouseY) ) {
+      bubbles[i].changeColor(255);
+    } else {
+      bubbles[i].changeColor(0);
+    }
     bubbles[i].move();
     bubbles[i].show();
   }
@@ -34,17 +46,31 @@ class Bubble {
     this.x = x;
     this.y = y;
     this.r = r;
+    this.brightness = 0;
+  }
+
+  changeColor(bright) {
+    this.brightness = bright;
+  }
+
+  contains(px, py) {
+    let d = dist(px, py, this.x, this.y);
+    if ( d < this.r ) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   move() {
-    this.x = this.x + random(-5, 5);
-    this.y = this.y + random(-5, 5);
+    this.x = this.x + random(-1, 1);
+    this.y = this.y + random(-1, 1);
   }
 
   show() {
     stroke(255);
-    strokeWeight(4);
-    noFill();
+    strokeWeight(2);
+    fill(this.brightness, 125);
     ellipse(this.x, this.y, this.r * 2);
   }
 }
