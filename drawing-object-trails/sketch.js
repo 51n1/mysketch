f63aab32ca4c8592e1958b0041c576196d9bf727
51n1 <>
2017-12-12 23:19:09 +0800

    2nd sketch of trails

--- drawing-object-trails/sketch.js
+++ drawing-object-trails/sketch.js
@@ -1,17 +1,22 @@
 // - 9.7: Drawing Object Trails
 // - p5.js Tutorial by Daniel Shiffman
-let gravity = 0.1;
-let particle;
+let particles = [];
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
-  particle = new Particle(100, 100);
+  //particle = new Particle(width/2, height/2);
+}
+
+function mousePressed() {
+  particles.push(new Particle(mouseX, mouseY));
 }
 
 function draw() {
   background(200);
-  particle.update();
-  particle.show();
+  for (let i = 0; i < particles.length; i++) {
+    particles[i].update();
+    particles[i].show();
+  }
 
   if (touches.length > 1 || keyIsDown(ENTER)) saveCanvas();
   // save(frameCount+".png");
@@ -21,23 +26,32 @@ class Particle {
   constructor(x_, y_) {
     this.x = x_;
     this.y = y_;
-    this.r = 10;
-    this.yspeed = 0;
+    this.r = 16;
+    this.history = [];
   }
 
   update() {
-    this.y += this.yspeed;
-    this.yspeed += gravity;
-    if (this.y > height) {
-      this.y = height;
-      this.yspeed *= -0.9;
+    this.x += random(-5, 5);
+    this.y += random(-5, 5);
+
+    let v = createVector(this.x, this.y);
+    this.history.push(v);
+
+    if (this.history.length > 25) {
+      this.history.splice(0, 1);
     }
   }
 
   show() {
     stroke(0);
-    noFill();
+    fill(0, 150);
     ellipse(this.x, this.y, this.r * 2);
+
+    for (let i = 0; i < this.history.length; i++) {
+      let pos = this.history[i];
+      fill(random(255));
+      ellipse(pos.x, pos.y, i, i);
+    }
   }
 }
 
