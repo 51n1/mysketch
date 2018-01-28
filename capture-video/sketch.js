36ea71f5d48f359568952022512d89e67fa58a77
51n1 <31303070+51n1@users.noreply.github.com>
2018-01-28 16:40:05 +0800

    Update sketch.js

--- capture-video/sketch.js
+++ capture-video/sketch.js
@@ -3,18 +3,19 @@ let capture;
 
 function setup() {
   // createCanvas(windowWidth, windowHeight);
-  createCanvas(640, 240);
+  createCanvas(320, 240);
   
   capture = createCapture(VIDEO);
   // createCapture(VIDEO);
-  capture.size(320, 240);
+  // capture.size(320, 240);
   // capture.hide();
 }
 
 function draw() {
   // background(100);
-  image(capture, 320, 0, 320, 240);
-  filter(INVERT);
+  tint(255, 0, 125);
+  image(capture, 0, 0, width, height);
+  //filter(INVERT);
   
   //if (touches.length > 1 || keyIsDown(ENTER)) saveCanvas();
   // save(frameCount+".png");
