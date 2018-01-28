6a46fc4c04c3c3b2ab289bef57e317b42183b224
51n1 <>
2018-01-28 21:22:58 +0800

    update

---
+++ capture-video-2/sketch.js
@@ -0,0 +1,30 @@
+
+let capture;
+
+function setup() {
+  // createCanvas(windowWidth, windowHeight);
+  createCanvas(320, 240);
+
+  capture = createCapture(VIDEO);
+  capture.size(320, 240);
+  // capture.hide();
+  createButton('snap');
+}
+
+function draw() {
+  // background(100);
+  // tint(255, 0, 125);
+  // image(capture, 0, 0, width, height);
+  // filter(INVERT);
+
+  //if (touches.length > 1 || keyIsDown(ENTER)) saveCanvas();
+  // save(frameCount+".png");
+}
+
+//function touchMoved() {
+//  return false;
+//}
+
+//function windowResized() {
+//  resizeCanvas(windowWidth, windowHeight);
+//}
