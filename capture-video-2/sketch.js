06bf9b6f664f153b61def6db0e3525dc3dd89697
51n1 <>
2018-01-28 21:48:01 +0800

    update

--- capture-video-2/sketch.js
+++ capture-video-2/sketch.js
@@ -1,10 +1,11 @@
 
 let capture;
 let button;
+let snapshots = [];
 
 function setup() {
   // createCanvas(windowWidth, windowHeight);
-  createCanvas(320, 240);
+  createCanvas(800, 240);
   background(100);
   capture = createCapture(VIDEO);
   capture.size(320, 240);
@@ -14,14 +15,15 @@ function setup() {
 }
 
 function takesnap() {
-  image(capture, 0, 0, width, height);
-}
-
-function mousePressed() {
-  image(capture, 0, 0);
+  snapshots.push(capture.get());
+  // image(capture, 0, 0, width, height);
 }
 
 function draw() {
+  for (let i = 0; i < snapshots.length; i++) {
+    tint(255, 50);
+    image(snapshots[i], 0, 0, capture.width, capture.height);
+  }
   // background(100);
   // tint(255, 0, 125);
   // image(capture, 0, 0, width, height);
