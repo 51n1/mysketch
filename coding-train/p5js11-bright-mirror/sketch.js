5d824b4376ef43e83240a2b0c2c2b50aa4cd232d
51n1 <>
2018-02-01 22:25:21 +0800

    up

--- coding-train/p5js11-bright-mirror/sketch.js
+++ coding-train/p5js11-bright-mirror/sketch.js
@@ -1,11 +1,12 @@
 // 11.4: Brightness Mirror - p5.js Tutorial by Daniel Shiffman
 let video;
+let vScale = 16;
 
 function setup() {
-  createCanvas(320, 240);
+  createCanvas(640, 480);
   pixelDensity(1);
   video = createCapture(VIDEO);
-  video.size(320, 240);
+  video.size(width/vScale, height/vScale);
 }
 
 function draw() {
@@ -14,32 +15,34 @@ function draw() {
   video.loadPixels();
   loadPixels();
 
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
-      let index = (x + y * width) * 4;
+  for (let y = 0; y < video.height; y++) {
+    for (let x = 0; x < video.width; x++) {
+      let index = (x + y * video.width) * 4;
       let r = video.pixels[index+0];
       let g = video.pixels[index+1];
       let b = video.pixels[index+2];
 
       let bright = (r+g+b)/3;
+      fill(bright);
+      rect(x*vScale, y*vScale, vScale, vScale);
 
-      pixels[index+0] = bright;
-      pixels[index+1] = bright;
-      pixels[index+2] = bright;
-      pixels[index+3] = 255;
+      // pixels[index+0] = bright;
+      // pixels[index+1] = bright;
+      // pixels[index+2] = bright;
+      // pixels[index+3] = 255;
     }
   }
 
-  updatePixels();
+  // updatePixels();
 
   if (touches.length > 1 || keyIsDown(ENTER)) saveCanvas();
   // save(frameCount+".png");
 }
 
-function touchMoved() {
-  return false;
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
+// function touchMoved() {
+//   return false;
+// }
+//
+// function windowResized() {
+//   resizeCanvas(windowWidth, windowHeight);
+// }
