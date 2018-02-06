5071811809a10c65f6adddd4030abf12bbe7ac11
51n1 <>
2018-02-07 00:56:03 +0800

    up

--- coding-train/p5js13-flow-field/sketch.js
+++ coding-train/p5js13-flow-field/sketch.js
@@ -1,32 +1,126 @@
 // Coding Challenge #24: Perlin Noise Flow Field - Perlin Noise and p5.js Tutorial by Daniel Shiffman
 
-let inc = 0.01;
-let scl = 20;
+let inc = 0.1;
+let scl = 10;
 let cols, rows;
+
+let zoff = 0;
+
+let fr;
+
+let particles = [];
+
+let flowfield;
 
 function setup() {
   // createCanvas(windowWidth, windowHeight);
-  createCanvas(200, 200);
+  createCanvas(800, 600);
   cols = floor(width / scl);
   rows = floor(height / scl);
+  fr = createP('');
+
+  flowfield = new Array(cols * rows);
+
+  for (let i = 0; i < 1500; i++) {
+    particles[i] = new Particle();
+  }
+  background(255);
 }
 
 function draw() {
+  randomSeed(10);
   let yoff = 0;
   for (let y = 0; y < rows; y++) {
     let xoff = 0;
     for (let x = 0; x < cols; x++) {
-      let index = (x + y * width) * 4;
-      let r = noise(xoff, yoff) * 255;
+      let index = x + y * cols;
+      let angle = noise(xoff, yoff, zoff) * TWO_PI * 4;
+      let v = p5.Vector.fromAngle(angle);
+      v.setMag(1);
+      flowfield[index] = v;
       xoff += inc;
-      fill(random(255));
-      rect(x * scl, y * scl, scl, scl);
+      // push();
+      // translate(x * scl, y * scl);
+      // rotate(v.heading());
+      // stroke(0, 50);
+      // strokeWeight(1);
+      // line(0, 0, scl, 0);
+      // pop();
     }
     yoff += inc;
+    zoff += 0.0003;
   }
+
+  for (let i = 0; i < particles.length; i++) {
+    particles[i].follow(flowfield);
+    particles[i].update();
+    particles[i].edges();
+    particles[i].show();
+  }
+
+  fr.html(floor(frameRate()));
 
   if (touches.length > 1 || keyIsDown(ENTER)) saveCanvas();
   // save(frameCount+".png");
+}
+
+function Particle() {
+  this.pos = createVector(random(width), random(height));
+  this.vel = createVector(0, 0);
+  this.acc = createVector(0, 0);
+  this.maxspeed = 4;
+
+  this.prevPos = this.pos.copy();
+
+  this.update = function() {
+    this.vel.add(this.acc);
+    this.vel.limit(this.maxspeed);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+
+  this.follow = function(vectors) {
+    let x = floor(this.pos.x / scl);
+    let y = floor(this.pos.y / scl);
+    let index = x + y * cols;
+    let force = vectors[index];
+    this.applyForce(force);
+  }
+
+  this.applyForce = function(force) {
+    this.acc.add(force);
+  }
+
+  this.show = function() {
+    stroke(0, 5);
+    strokeWeight(1);
+    line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y)
+    this.updatePrev();
+  }
+
+  this.updatePrev = function() {
+    this.prevPos.x = this.pos.x;
+    this.prevPos.y = this.pos.y;
+  }
+
+  this.edges = function() {
+    if (this.pos.x > width) {
+      this.pos.x = 0;
+      this.updatePrev();
+    }
+    if (this.pos.x < 0) {
+      this.pos.x = width;
+      this.updatePrev();
+    }
+    if (this.pos.y > height) {
+      this.pos.y = 0;
+      this.updatePrev();
+    }
+    if (this.pos.y < 0) {
+      this.pos.y = height;
+      this.updatePrev();
+    }
+  }
 }
 
 function touchMoved() {
@@ -34,5 +128,5 @@ function touchMoved() {
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(width, height);
 }
