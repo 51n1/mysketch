56b522c240b8ca55c455287dcf454955b661a96c
51n1 <>
2018-02-10 00:41:01 +0800

    rename

---
+++ coding-train/cc-36-blobby/sketch.js
@@ -0,0 +1,34 @@
+// - Coding Challenge #36: Blobby! by Daniel Shiffman
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0);
+
+  if (touches.length > 1 || keyIsDown(ENTER)) saveCanvas();
+  // save(frameCount+".png");
+}
+
+class Circle {
+  constructor(x_, y_) {
+    this.x = x_;
+    this.y = y_;
+    this.r = 1;
+  }
+
+  show() {
+    stroke(255);
+    noFill();
+    ellipse(this.x, this.y, this.r * 2);
+  }
+}
+
+function touchMoved() {
+  return false;
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
