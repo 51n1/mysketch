367e0e9a942729f44fe80ba539f924b2f4126425
51n1 <mail4.51n1@gmail.com>
2018-03-26 22:09:32 +0800

    update

--- coding-train/p5js17-9-sound-visual/sketch.js
+++ coding-train/p5js17-9-sound-visual/sketch.js
@@ -2,23 +2,53 @@
 
 let song;
 let amp;
-let history = [];
+let volhistory = [];
+let button;
 
 function preload() {
   song = loadSound("Futura.mp3");
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  song.play();
+  let cnv = createCanvas(windowWidth, 300);
+  cnv.parent("sketch_canvas");
+
+  button = createButton("toggle");
+  button.parent("play_button");
+  button.mousePressed(toggleSong);
+
   amp = new p5.Amplitude();
+}
+
+function toggleSong() {
+  if (!song.isPlaying()) {
+    song.play();
+  } else {
+    song.pause();
+  }
 }
 
 function draw() {
   background(0);
   let vol = amp.getLevel();
-  history.push(vol);
-  ellipse(width/2, height/2, vol * width, vol * height);
+  volhistory.push(vol);
+  stroke(255);
+  noFill();
+  beginShape();
+  for (let i = 0; i < volhistory.length; i++) {
+    let y = map(volhistory[i], 0, 1, height, 0);
+    vertex(i, y);
+  }
+  endShape();
+
+  if (volhistory.length > width - 50) {
+    volhistory.splice(0, 1);
+  }
+
+  stroke(255, 0, 0);
+  line(volhistory.length, 0, volhistory.length, height);
+
+  // ellipse(width/2, height/2, vol * width, vol * height);
 
   if (touches.length > 1 || keyIsDown(ENTER)) saveCanvas();
   // save(frameCount+".png");
