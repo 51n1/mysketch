3279db86de2b52c83a2f8835f4a4f487dd54c508
51n1 <mail4.51n1@gmail.com>
2018-04-01 22:15:59 +0800

    update

--- coding-train/p5js18-2-3d-geometries/sketch.js
+++ coding-train/p5js18-2-3d-geometries/sketch.js
@@ -1,4 +1,5 @@
 // 18.2: 3D Geometries - WebGL and p5.js Tutorial by Daniel Shiffman
+// 18.3: Light and Material
 
 let angle = 0;
 
@@ -7,20 +8,27 @@ function setup() {
 }
 
 function draw() {
-  background(175);
+  let dx = mouseX - width / 2;
+  let dy = mouseY - height / 2;
+  let v = createVector(dx,dy,0);
+  v.normalize();
+  ambientLight(128, 0, 0);
+  pointLight(0, 0, 255, -1000, 0, 1000);
+  directionalLight(0, 255, 255, v);
 
-  rectMode(CENTER);
-  fill(0, 0, 255);
-  noStroke();
+  background(175);
   // translate(0, 0, mouseY);
   rotateX(angle);
   rotateY(angle * 0.3);
   rotateZ(angle * 1.2);
-  // rect(0, 0, 150, 150);
 
-  torus(100, 30);
+  noStroke();
+  // normalMaterial();
+  ambientMaterial(255);
+  // specularMaterial(255);
+  torus(150, 25);
 
-  angle += 0.03;
+  angle += 0.02;
 
   if (touches.length > 1 || keyIsDown(ENTER)) saveCanvas();
   // save(frameCount+".png");
