696b1ff3c8ed3f39dbf465f8eab9b6c93de70b5d
51n1 <mail4.51n1@gmail.com>
2018-04-02 10:43:52 +0800

    update

--- coding-train/p5js18-2-3d-geometries/sketch.js
+++ coding-train/p5js18-2-3d-geometries/sketch.js
@@ -1,22 +1,33 @@
-// 18.2: 3D Geometries - WebGL and p5.js Tutorial by Daniel Shiffman
+// WebGL and p5.js Tutorial by Daniel Shiffman
+// 18.2: 3D Geometries
 // 18.3: Light and Material
+// 18.4: 
 
 let angle = 0;
+let img;
+
+function preload() {
+  img = loadImage("red.jpeg");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
 }
 
 function draw() {
-  let dx = mouseX - width / 2;
-  let dy = mouseY - height / 2;
-  let v = createVector(dx,dy,0);
-  v.normalize();
-  ambientLight(128, 0, 0);
-  pointLight(0, 0, 255, -1000, 0, 1000);
-  directionalLight(0, 255, 255, v);
-
   background(175);
+  // let dx = mouseX - width / 2;
+  // let dy = mouseY - height / 2;
+  // let v = createVector(dx, dy, 0);
+  // v.normalize();
+  // v.div(100);
+  ambientLight(255);
+  // pointLight(0, 0, 255, -1000, 0, 1000);
+  // directionalLight(255, 255, 255, v);
+  directionalLight(0, 0, 255, 1, 0, 0);
+  directionalLight(0, 255, 0, 0, 1, 0);
+  directionalLight(255, 0, 0, 0, 0, -1);
+
   // translate(0, 0, mouseY);
   rotateX(angle);
   rotateY(angle * 0.3);
@@ -24,9 +35,13 @@ function draw() {
 
   noStroke();
   // normalMaterial();
-  ambientMaterial(255);
+  // ambientMaterial(255);
   // specularMaterial(255);
-  torus(150, 25);
+  texture(img);
+  // torus(150, 25);
+  // plane(300, 300);
+  // sphere(200);
+  box(200);
 
   angle += 0.02;
 
