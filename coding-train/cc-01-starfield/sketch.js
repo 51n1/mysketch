b3b3fc5acd45ac05b3c6b650e64d716e8a2fae6a
51n1 <mail4.51n1@gmail.com>
2018-05-05 23:27:06 +0800

    add

---
+++ coding-train/cc-01-starfield/sketch.js
@@ -0,0 +1,71 @@
+// Reference - Coding Challenge #1: Starfield in Processing by Daniel Shiffman
+
+let stars = [];
+let speed;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  for (let i = 0; i < 600; i++) {
+    let star = new Star();
+    stars.push(star);
+  }
+}
+
+function draw() {
+  speed = map(mouseX, 0, width, 0, 50);
+  background(0);
+  translate(width/2, height/2);
+  for (let i = 0; i < stars.length; i++) {
+    stars[i].update();
+    stars[i].show();
+  }
+
+  if (touches.length > 1 || keyIsDown(ENTER)) saveCanvas();
+  // save(frameCount+".png");
+}
+
+class Star {
+  constructor() {
+    this.x = random(-width, width);
+    this.y = random(-height, height);
+    this.z = random(width);
+    this.pz = this.z;
+  }
+
+  update() {
+    this.z = this.z - speed;
+    if (this.z < 1) {
+      this.z = width;
+      this.x = random(-width, width);
+      this.y = random(-height, height);
+      this.pz = this.z;
+    }
+  }
+
+  show() {
+    fill(255);
+    noStroke();
+
+    let sx = map(this.x / this.z, 0, 1, 0, width);
+    let sy = map(this.y / this.z, 0, 1, 0, height);
+
+    let r = map(this.z, 0, width, 16, 0);
+    // ellipse(sx, sy, r);
+
+    let px = map(this.x / this.pz, 0, 1, 0, width);;
+    let py = map(this.y / this.pz, 0, 1, 0, height);
+
+    this.pz = this.z;
+
+    stroke(255);
+    line(px, py, sx, sy);
+  }
+}
+
+function touchMoved() {
+  return false;
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
