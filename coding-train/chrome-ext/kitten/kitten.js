84a22246c32e60e7cf06b7ff40b2ff1937e2e785
51n1 <mail4.51n1@gmail.com>
2018-05-24 22:31:04 +0800

    add

---
+++ coding-train/chrome-ext/kitten/kitten.js
@@ -0,0 +1,21 @@
+console.log("Kittens of the world, unite as one!");
+
+// brenda-helen-17456-unsplash.jpg
+// david-clode-547746-unsplash.jpg
+// tim-mossholder-594536-unsplash.jpg
+
+let filenames = [
+  "brenda-helen-17456-unsplash.jpg",
+  "david-clode-547746-unsplash.jpg",
+  "tim-mossholder-594536-unsplash.jpg"
+];
+
+let imgs = document.getElementsByTagName('img');
+
+for (imgElt of imgs) {
+  let r = Math.floor(Math.random() * filenames.length);
+  let file = "kittens/" + filenames[r];
+  let url = chrome.extension.getURL(file);
+  imgElt.src = url;
+  console.log(url);
+}
