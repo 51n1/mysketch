4794d7b6ff2c99d34367ace6cac4d61f9b11f74c
51n1 <31303070+51n1@users.noreply.github.com>
2021-02-20 01:20:09 +0800

    update sketch

--- pcd2021-starfield-rainbow/sketch.js
+++ pcd2021-starfield-rainbow/sketch.js
@@ -3,13 +3,13 @@
 // https://youtu.be/17WoOqgXsRM
 
 let stars = [];
-let speed = 20;
+let speed = 50;
 
 function setup() {
-  createCanvas(1000, 500);
-  // createCanvas(windowWidth, windowHeight);
+  // createCanvas(1000, 500);
+  createCanvas(windowWidth, windowHeight);
 
-  let maxStarts = int(width) * 10;
+  let maxStarts = int(width) * 2;
 
   for (let i = 0; i < maxStarts; i++) {
     let star = new Star();
@@ -17,24 +17,22 @@ function setup() {
   }
 
   colorMode(HSB, 255);
+  drawingContext.shadowOffsetX = 5;
+  drawingContext.shadowOffsetY = random(10);
+  drawingContext.shadowBlur = random(10);
+  drawingContext.shadowColor = "rgba(255, 0, 255, .5)";
+  // gradient = drawingContext.createLinearGradient(width/2, 0, width/2, height);
+  gradient = drawingContext.createRadialGradient(width/2, height/2, 50, width/2, height/2, height/2);
+  gradient.addColorStop(0, "rgba(200, 250, 250, .5)");
+  gradient.addColorStop(.99, "rgba(0, 30, 30, .3)");
+  // gradient.addColorStop(1, "rgba(0, 0, 255, .5)");
 
   // noLoop();
 }
 
 function draw() {
   //if (mouseX) speed = map(mouseX, 0, width, 20, 50);
-
   background(0);
-  drawingContext.shadowOffsetX = 5;
-  drawingContext.shadowOffsetY = random(10);
-  drawingContext.shadowBlur = random(10);
-  drawingContext.shadowColor = "rgba(0, 255, 255, .5)";
-
-  // gradient = drawingContext.createLinearGradient(width/2, 0, width/2, height);
-  gradient = drawingContext.createRadialGradient(width/2, height/2, 50, width/2, height/2, height/2);
-  gradient.addColorStop(0, "rgba(200, 255, 255, .5)");
-  gradient.addColorStop(.99, "rgba(10, 10, 50, .5)");
-  // gradient.addColorStop(1, "rgba(0, 0, 255, .5)");
   drawingContext.fillStyle = gradient;
   drawingContext.fillRect(0, 0, width, height);
 
@@ -80,7 +78,7 @@ class Star {
       col_r = map(this.y, 0, -height, 0, 255);
     }
 
-    stroke(col_r, 255, 255, 255);
+    stroke(col_r, 100, 255, 255);
 
     line(px, py, sx, sy);
   }
